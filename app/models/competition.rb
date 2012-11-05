a0ae2bcd66b199697c6266d542ddb889fc7b2b39
gursoy <gursoy@yerli.org>
2012-11-04 21:45:06 -0600

    competitions and all the lookups done

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,12 +1,13 @@
 class Competition < ActiveRecord::Base
+  attr_accessible :competition_name, :competition_format_id, :competition_type_id, :area_id, :soccer_type_id, :display_order_num, :team_type_id, :source_system_id, :source_system_competition_key
 
 
+  belongs_to :area, :class_name => 'Area', :foreign_key => :area_id
+  has_many :seasons, :class_name => 'Season'
+  belongs_to :source_system, :class_name => 'SourceSystem', :foreign_key => :source_system_id
+  belongs_to :competition_format, :class_name => 'CompetitionFormat', :foreign_key => :competition_format_id
+  belongs_to :competition_type, :class_name => 'CompetitionType', :foreign_key => :competition_type_id
+  belongs_to :soccer_type, :class_name => 'SoccetType', :foreign_key => :soccer_type_id
+  belongs_to :team_type, :class_name => 'TeamType', :foreign_key => :team_type_id
 
-    belongs_to :area, :class_name => 'Area', :foreign_key => :area_id    
-    has_many :seasons, :class_name => 'Season'    
-    belongs_to :source_system, :class_name => 'SourceSystem', :foreign_key => :source_system_id    
-    belongs_to :competition_format, :class_name => 'CompetitionFormat', :foreign_key => :competition_format_id    
-    belongs_to :competition_type, :class_name => 'CompetitionType', :foreign_key => :competition_type_id    
-    belongs_to :soccer_type, :class_name => 'SoccetType', :foreign_key => :soccer_type_id
-    belongs_to :team_type, :class_name => 'TeamType', :foreign_key => :team_type_id    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@
 #CompetitionFormat.create([{competition_format_code: 'ICUP',competition_format_name: 'International cup'}])
 #CompetitionFormat.create([{competition_format_code: 'DSCP',competition_format_name: 'Domestic super cup'}])
 
-CompetitionType.create([{competition_type_code: 'CLUB', competition_type_name: 'Club'}])
-CompetitionType.create([{competition_type_code: 'INTRN', competition_type_name: 'International'}])
+#CompetitionType.create([{competition_type_code: 'CLUB', competition_type_name: 'Club'}])
+#CompetitionType.create([{competition_type_code: 'INTRN', competition_type_name: 'International'}])
 
-SoccerType.create([{soccer_type_code: 'CUP', soccer_type_name: 'Cup'}])
-SoccerType.create([{soccer_type_code: 'TBL', soccer_type_name: 'Table'}])
+#SoccerType.create([{soccer_type_code: 'CUP', soccer_type_name: 'Cup'}])
+#SoccerType.create([{soccer_type_code: 'TBL', soccer_type_name: 'Table'}])
 
-TeamType.create([{team_type_code: 'DEFAULT', team_type_name: 'Default'}])
-TeamType.create([{team_type_code: 'YOUTH', team_type_name: 'Youth'}])
-TeamType.create([{team_type_code: 'WOMEN', team_type_name: 'Women'}])
+#TeamType.create([{team_type_code: 'DEFAULT', team_type_name: 'Default'}])
+#TeamType.create([{team_type_code: 'YOUTH', team_type_name: 'Youth'}])
+#TeamType.create([{team_type_code: 'WOMEN', team_type_name: 'Women'}])
 
