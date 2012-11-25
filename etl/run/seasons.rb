ddfb7c6102c2ab5c38e1f9f6a4005974cb242086
gursoy <gursoy@yerli.org>
2012-11-25 01:35:52 -0600

    matches in

--- etl/run/seasons.rb
+++ etl/run/seasons.rb
@@ -7,25 +7,46 @@ require 'pg'
 require 'yaml'
 require 'date'
 
-def parseMatches (matches)
-  matches.css('div.content-column div div div div.table-container table tbody tr').each do |row|
-    date_time = DateTime.strptime(row['data-timestamp'],'%s')
-    source_system_match_id = row['id'].split('-')[1]
-    team_a = row.css('td')[2].text.strip
-    team_b = row.css('td')[4].text.strip
-    team_a_score = row.css('td')[3].text.strip.delete(' ').split('-')[0]
-    team_b_score = row.css('td')[3].text.strip.delete(' ').split('-')[1]
-    match_details_url = row.css('td')[3].css('a').attribute('href').text
-    return "match_id=#{source_system_match_id}|date=#{date_time}|A=#{team_a}|B=#{team_b}|a_score=#{team_a_score}|b_score=#{team_b_score}|URL=#{match_details_url}"
-  end #matches.css('div.table-container table tbody tr').each do |row|
-end
 
+def parseMatches (browser, season_name, round_name, group_name) #(matches)
+  line = ""
+  click_away = true
+  
+  while click_away do
+    matches = Nokogiri::HTML(browser.html)
+    #matches.css('div.content-column div div div div.table-container table tbody tr').each do |row|
+    matches.css('div.table-container table tbody tr').each do |row|      
+      date_time = DateTime.strptime(row['data-timestamp'],'%s')
+      source_system_match_id = row['id'].split('-')[1]
+      team_a = row.css('td')[2].text.strip
+      team_b = row.css('td')[4].text.strip
+      team_a_score = row.css('td')[3].text.strip.delete(' ').split('-')[0]
+      team_b_score = row.css('td')[3].text.strip.delete(' ').split('-')[1]
+      match_details_url = row.css('td')[3].css('a').attribute('href').text
+      line =  "#{line}#{season_name}|#{round_name}|#{group_name}|#{source_system_match_id}|#{date_time}|#{team_a}|#{team_b}|#{team_a_score}|#{team_b_score}|#{match_details_url}\n"
+      #puts line
+    end #matches.css('div.table-container table tbody tr').each do |row|
+    #pp matches.css('span.nav_description a')
+    #puts matches.css('span.nav_description a').attribute('class')
+    if matches.css('span.nav_description a').to_s.strip != ''
+      if matches.css('span.nav_description a').attribute('class').to_s.strip != "previous"
+        click_away = false
+      else
+        browser.a(:class => "previous").click
+        sleep 1      
+      end #matches.css('span.nav_description a').attribute('class').to_s.strip != "previous"
+    else
+      click_away = false
+    end
+  end #while
+  puts line
+  return line
+end
 
 browser = Watir::Browser.new :chrome
 url = "http://www.soccerway.com/international/europe/european-championships"
-click_away = true
+#url = "http://www.soccerway.com/national/turkey/super-lig/2010-2011/regular-season"
 matches = nil
-
 
 width = browser.execute_script("return screen.width;")
 height = browser.execute_script("return screen.height;")
@@ -33,29 +54,45 @@ browser.driver.manage.window.move_to(0,0)
 browser.driver.manage.window.resize_to(width,height)
 browser.goto url
 
+file = File.new 'matches.dat', 'w'
+file.write "season_name|round_name|group_name|match_id|date|team_a|team_b|team_a_score|team_b_score|match_details_url\n"
+
 seasons = Nokogiri::HTML(browser.html)
 seasons.css("#season_id_selector option").each do |season|
   puts "season=#{season.content}"
   browser.select_list(:name => 'season_id').select season.content
   rounds = Nokogiri::HTML(browser.html)
-  rounds.css("#round_id_selector option").each do |round|
-    puts "round=#{round.content}"
-    browser.select_list(:name => 'round_id').select round.content
-    groups = Nokogiri::HTML(browser.html)
-    if browser.select_list(:id => "group_id_selector").exists? then
-      puts "Groups exists."
-      groups.css("#group_id_selector option").each do |group|
-        if group.content.to_s != "" then
-          puts "group=#{group.content}"
-          browser.select_list(:name => 'group_id').select group.content
-          puts parseMatches Nokogiri::HTML(browser.html)
-          
-        end #if group.content.to_s != "" then
-      end #groups.css("#group_id_selector option").each do |group|
-    else
-      puts parseMatches groups
-    end #browser.select_list(:id => "group_id_selector").exists?
-  end #rounds.css("#round_id_selector option").each do |round|
+  if browser.select_list(:id => "round_id_selector").exists? then
+    puts "Rounds exists."
+    rounds.css("#round_id_selector option").each do |round|
+      puts "round=#{round.content}"
+      browser.select_list(:name => 'round_id').select round.content
+      groups = Nokogiri::HTML(browser.html)
+      if browser.select_list(:id => "group_id_selector").exists? then
+        puts "Groups exists."
+        groups.css("#group_id_selector option").each do |group|
+          if group.content.to_s != "" then
+            puts "group=#{group.content}"
+            browser.select_list(:name => 'group_id').select group.content
+            #ln = parseMatches Nokogiri::HTML(browser.html)
+            ln = parseMatches browser, season.content, round.content, group.content 
+            #puts ln
+            file.write ln          
+          end #if group.content.to_s != "" then
+        end #groups.css("#group_id_selector option").each do |group|
+      else
+        #ln = parseMatches groups
+        ln = parseMatches browser, season.content, round.content, nil
+        #puts ln
+        file.write ln
+      end #browser.select_list(:id => "group_id_selector").exists?
+    end #rounds.css("#round_id_selector option").each do |round|
+  else
+    #ln = parseMatches seasons
+    ln = parseMatches browser, season.content, nil, nil
+    #puts ln
+    file.write ln    
+  end #if browser.select_list(:id => "round_id_selector").exists? then
 end #seasons.css("#season_id_selector option").each do |season|
 
 browser.close
