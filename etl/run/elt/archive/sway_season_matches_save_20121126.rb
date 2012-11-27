8f311d1e78c3c2962aff59ce23bf7b938f6f690a
gursoy <gursoy@yerli.org>
2012-11-27 00:25:07 -0600

    match bugs crushed

--- etl/run/elt/archive/sway_season_matches_save_20121126.rb
+++ etl/run/elt/archive/sway_season_matches_save_20121126.rb
@@ -10,7 +10,7 @@ require 'yaml'
 require 'date'
 
 
-def parseMatches (browser, competitionID, season_name, round_name, group_name) #(matches)
+def parseMatches (browser, season_name, round_name, group_name) #(matches)
   line = ""
   match = ""
   click_away = true
@@ -25,51 +25,17 @@ def parseMatches (browser, competitionID, season_name, round_name, group_name) #
         next
       end
      
-      #pp row
-      a = row.css('td.score-time a').text.strip.gsub(' ','').split("\n")
-=begin
-      This is resulting array of split above   
-      ["4-0"]
-      ["P", "0-0", "P"]
-      ["1-2"]
-      ["0-1"]
-      ["4-2"]
-      ["2-0"]
-      ["P", "0-0", "P"]
-=end      
-      #puts row.css('td.score-time a').text.strip.gsub("\n",'').gsub(' ','')
-      final_result = ''
-      if a.size > 1 #when penalties or extra time (P or E)
-        result_type = a[0]
-        final_result = 'A' if row.css('td.score-time a span.addition-visible').attribute('class').text.include?('left')        
-        final_result = 'B' if row.css('td.score-time a span.addition-visible').attribute('class').text.include?('right')
-        score = a[1]
-      else
-        result_type = 'R' #after regulation
-        score = a[0]
-      end
-      
-      #puts "Score=#{score}"
-      reg_result = case
-                     when score.to_s == '-' || score.to_s.include?(':') || score.to_s.include?('PSTP') || score.to_s.include?('CANC') then next 
-                     when score.split('-')[0] > score.split('-')[1] then reg_result = 'A'       
-                     when score.split('-')[0] < score.split('-')[1] then reg_result = 'B'
-                     when score.split('-')[0] = score.split('-')[1] then reg_result = 'D'
-                   end
-                         
       header = row.parent.parent.parent.parent.parent.parent.css('h2').text
       date_time = DateTime.strptime(row['data-timestamp'],'%s')
       source_system_match_id = row['id'].split('-')[1]
       team_a = row.css('td')[2].text.strip
       team_b = row.css('td')[4].text.strip
-      team_a_score = score.split('-')[0]
-      team_b_score = score.split('-')[1]
+      team_a_score = row.css('td')[3].text.strip.delete(' ').split('-')[0]
+      team_b_score = row.css('td')[3].text.strip.delete(' ').split('-')[1]
       match_details_url = row.css('td')[3].css('a').attribute('href').text
-      match = "#{competitionID}|#{header}|#{season_name}|#{round_name}|#{group_name}|#{source_system_match_id}|#{date_time}|#{team_a}|#{team_b}|#{team_a_score}|#{team_b_score}|#{result_type}|#{reg_result}|#{final_result}|#{match_details_url}"
+      match = "#{header}|#{season_name}|#{round_name}|#{group_name}|#{source_system_match_id}|#{date_time}|#{team_a}|#{team_b}|#{team_a_score}|#{team_b_score}|#{match_details_url}"
       line = "#{line}#{match}\n"
-      puts match     
-    
-
+      puts match
     end #matches.css('div.table-container table tbody tr').each do |row|
     
     if matches.css('span.nav_description a').to_s.strip != ''
@@ -119,19 +85,17 @@ year = time.year
 month = "%02d" % time.month
 day = "%02d" % time.day
 ts = "#{year}#{month}#{day}"
-season_threshold = '1999/2000'
-
 
 matchFile = File.new outputFile, 'w'
 
 #write header
-matchFile.write "competition_id|header|season_name|round_name|group_name|match_id|date|team_a|team_b|team_a_score|team_b_score|result_type|reg_result|final_result|match_details_url\n"
+matchFile.write "header|season_name|round_name|group_name|match_id|date|team_a|team_b|team_a_score|team_b_score|match_details_url\n"
 
 seasons = Nokogiri::HTML(browser.html)
 
 seasons.css("#season_id_selector option").each do |season|
 
-  exit if season.content == season_threshold
+  #exit if season.content == '2010/2011'
   
   puts "season=#{season.content}"
   browser.select_list(:name => 'season_id').select season.content
@@ -157,7 +121,7 @@ seasons.css("#season_id_selector option").each do |season|
              
             puts "group=#{group.content}"
             browser.select_list(:name => 'group_id').select group.content
-            ln = parseMatches browser, competitionID, season.content, round.content, group.content 
+            ln = parseMatches browser, season.content, round.content, group.content 
             matchFile.write ln
                       
           end #if group.content.to_s != ""
@@ -166,7 +130,7 @@ seasons.css("#season_id_selector option").each do |season|
         
       else #group doesn't exists
         
-        ln = parseMatches browser, competitionID, season.content, round.content, nil
+        ln = parseMatches browser, season.content, round.content, nil
         matchFile.write ln
         
       end #browser.select_list(:id => "group_id_selector").exists?
@@ -186,7 +150,7 @@ seasons.css("#season_id_selector option").each do |season|
            
           puts "group=#{group.content}"
           browser.select_list(:name => 'group_id').select group.content
-          ln = parseMatches browser, competitionID, season.content, nil, group.content 
+          ln = parseMatches browser, season.content, nil, group.content 
           matchFile.write ln
                     
         end #if group.content.to_s != ""
@@ -194,7 +158,7 @@ seasons.css("#season_id_selector option").each do |season|
       end 
     else #only season exists, no round no group
       
-      ln = parseMatches browser, competitionID, season.content, nil, nil
+      ln = parseMatches browser, season.content, nil, nil
       matchFile.write ln
             
     end #browser.select_list(:id => "group_id_selector").exists?
--- etl/run/test_multi.rb
+++ etl/run/test_multi.rb
@@ -20,22 +20,44 @@ def parseMatches (browser, season_name, round_name, group_name) #(matches)
     matches = Nokogiri::HTML(browser.html)
     
     matches.css('div.table-container table tbody tr').each do |row|
-      if row.css('td')[0].text == 'Aggr.' 
-        next
-      end      
-      ss = row.parent.parent.parent.parent.parent.parent.css('h2').text
-      if ss != season_name 
-        next
+      #pp row
+      a = row.css('td.score-time a').text.strip.gsub(' ','').split("\n")
+=begin
+      This is resulting array of split above   
+      ["4-0"]
+      ["P", "0-0", "P"]
+      ["1-2"]
+      ["0-1"]
+      ["4-2"]
+      ["2-0"]
+      ["P", "0-0", "P"]
+=end      
+      #puts row.css('td.score-time a').text.strip.gsub("\n",'').gsub(' ','')
+      final_result = ''
+      if a.size > 1 #when penalties or extra time (P or E)
+        result_type = a[0]
+        final_result = 'A' if row.css('td.score-time a span.addition-visible').attribute('class').text.include?('left')        
+        final_result = 'B' if row.css('td.score-time a span.addition-visible').attribute('class').text.include?('right')
+        score = a[1]
+      else
+        result_type = 'R' #after regulation
+        score = a[0]
       end
+      reg_result = case
+                     when score.split('-')[0] > score.split('-')[1] then reg_result = 'A'       
+                     when score.split('-')[0] < score.split('-')[1] then reg_result = 'B'
+                     when score.split('-')[0] = score.split('-')[1] then reg_result = 'D'
+                   end
       date_time = DateTime.strptime(row['data-timestamp'],'%s')
       source_system_match_id = row['id'].split('-')[1]
       team_a = row.css('td')[2].text.strip
       team_b = row.css('td')[4].text.strip
-      team_a_score = row.css('td')[3].text.strip.delete(' ').split('-')[0]
-      team_b_score = row.css('td')[3].text.strip.delete(' ').split('-')[1]
+      team_a_score = score.split('-')[0]
+      team_b_score = score.split('-')[1]
       match_details_url = row.css('td')[3].css('a').attribute('href').text
-      line =  "#{line}#{team_a}|#{team_b}|#{team_a_score}|#{team_b_score}\n"
-   
+      match = "#{season_name}|#{round_name}|#{group_name}|#{source_system_match_id}|#{date_time}|#{team_a}|#{team_b}|#{team_a_score}|#{team_b_score}|#{result_type}|#{reg_result}|#{final_result}|#{match_details_url}"
+      line = "#{line}#{match}\n"
+      puts match         
     end #matches.css('div.table-container table tbody tr').each do |row|
     
     if matches.css('span.nav_description a').to_s.strip != ''
@@ -56,11 +78,12 @@ def parseMatches (browser, season_name, round_name, group_name) #(matches)
       click_away = false
       
     end
+  
     
   end #while
   
-  puts line
-  return line
+#  puts line
+#  return line
   
 end #parseMatches
 
@@ -70,19 +93,7 @@ outputFile = 'matches.dat'
 browser = Watir::Browser.new :chrome
 browser.goto competitionURL
 
-time = Time.new
-year = time.year
-month = "%02d" % time.month
-day = "%02d" % time.day
-ts = "#{year}#{month}#{day}"
-
-matchFile = File.new outputFile, 'w'
-
-#write header
-matchFile.write "season_name|round_name|group_name|match_id|date|team_a|team_b|team_a_score|team_b_score|match_details_url\n"
-
-ln = parseMatches browser, "2nd Phase Reválida 2 - Finals", 'B', 'C' 
-matchFile.write ln
+parseMatches browser, nil, nil, nil 
 
 browser.close
 exit
